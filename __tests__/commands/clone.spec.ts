682087a605d8e4dc82a54abbab5143d17dc3c824
Bruno Rocha <brunorocha2674@gmail.com>
2023-03-13 18:05:17 -0300

    test: clone command

---
+++ __tests__/commands/clone.spec.ts
@@ -0,0 +1,94 @@
+import { cloneCommand } from '@/commands'
+import { cwd } from '@/utils/constants'
+import { writeLockfile } from '@/utils/file-system'
+import { mockParams } from '@/tests/mocks/mock-params'
+import cp from 'node:child_process'
+import { existsSync, mkdirSync, rmdirSync } from 'node:fs'
+import { join } from 'node:path'
+import axios from 'axios'
+import * as p from '@clack/prompts'
+
+const repo = {
+  name: 'my-cli',
+  clone_url: 'https://github.com/Mist3rBru/my-cli.git',
+  updated_at: new Date()
+}
+
+jest.mock('@clack/prompts', () => ({
+  select: jest.fn(async () => repo)
+}))
+
+jest.mock('axios', () => ({
+  get: jest.fn(async () => ({
+    data: [
+      { updated_at: new Date().setDate(-1) },
+      { updated_at: new Date().setDate(1) },
+      { updated_at: new Date().setDate(-1) },
+      repo
+    ]
+  }))
+}))
+
+describe('cloneCommand', () => {
+  beforeAll(() => {
+    writeLockfile({
+      git: 'Mist3rBru'
+    })
+    jest.spyOn(global.process, 'exit').mockImplementation(() => ({} as never))
+    jest.spyOn(cp, 'execSync').mockImplementation(() => ({} as never))
+  })
+
+  it('should get github repositories', async () => {
+    await cloneCommand()
+
+    expect(axios.get).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return error on invalid repository name', async () => {
+    mockParams(['your-cli'])
+
+    await cloneCommand()
+
+    expect(process.exit).toHaveBeenCalledTimes(1)
+  })
+
+  it('should clone on valid repository', async () => {
+    mockParams(['my-cli'])
+
+    await cloneCommand()
+
+    expect(cp.execSync).toHaveBeenCalledTimes(1)
+    expect(cp.execSync).toHaveBeenCalledWith(
+      [
+        `git clone ${repo.clone_url} ${repo.name}`,
+        `cd ${repo.name}`,
+        'git remote rename origin o',
+        'pnpm install',
+        'code .'
+      ].join(' && '),
+      expect.anything()
+    )
+  })
+
+  it('should still prepare pre-cloned repository', async () => {
+    const repositoryPath = join(cwd, 'my-cli')
+    if (!existsSync(repositoryPath)) mkdirSync(repositoryPath)
+
+    await cloneCommand()
+
+    expect(cp.execSync).toHaveBeenCalledTimes(1)
+    expect(cp.execSync).toHaveBeenCalledWith(
+      [`cd ${repo.name}`, 'pnpm install', 'code .'].join(' && '),
+      expect.anything()
+    )
+    rmdirSync(repositoryPath)
+  })
+
+  it('should render prompts', async () => {
+    mockParams([])
+
+    await cloneCommand()
+
+    expect(p.select).toHaveBeenCalledTimes(1)
+  })
+})
