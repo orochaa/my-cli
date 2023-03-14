dd9e24578225b9fa8fde0afbea2bb3f55bc2f832
Bruno Rocha <brunorocha2674@gmail.com>
2023-03-14 13:06:11 -0300

    test: run command

---
+++ __tests__/commands/run.spec.ts
@@ -0,0 +1,55 @@
+import { clearParams, mockParams } from '@/tests/mocks/mock-params'
+import { runCommand } from '@/commands'
+import cp from 'node:child_process'
+
+jest.spyOn(global.process, 'exit').mockImplementation(() => ({} as never))
+
+jest.mock('@clack/prompts', () => ({
+  multiselect: jest.fn(async () => ['lint', 'build'])
+}))
+
+jest.spyOn(cp, 'execSync').mockImplementation(() => ({} as any))
+
+describe('runCommand', () => {
+  beforeEach(() => {
+    clearParams()
+  })
+
+  it("should run prompt's selected scripts", async () => {
+    await runCommand()
+
+    expect(cp.execSync).toHaveBeenCalledTimes(2)
+    expect(cp.execSync).toHaveBeenCalledWith('npm run lint', expect.anything())
+    expect(cp.execSync).toHaveBeenCalledWith('npm run build', expect.anything())
+  })
+
+  it("should run prompt's selected scripts", async () => {
+    mockParams('lint', 'build', 'test')
+
+    await runCommand()
+
+    expect(cp.execSync).toHaveBeenCalledTimes(3)
+    expect(cp.execSync).toHaveBeenCalledWith('npm run lint', expect.anything())
+    expect(cp.execSync).toHaveBeenCalledWith('npm run build', expect.anything())
+    expect(cp.execSync).toHaveBeenCalledWith('npm run test', expect.anything())
+  })
+
+  it("should verify package.json scripts", async () => {
+    jest.spyOn(JSON,'parse').mockReturnValueOnce({})
+    
+    await runCommand()
+
+    expect(process.exit).toHaveBeenCalledTimes(1)
+  })
+
+  it("should verify if script exists in package.json", async () => {
+    jest.spyOn(JSON,'parse').mockReturnValueOnce({
+      scripts: {}
+    })
+    mockParams('lint')
+    
+    await runCommand()
+
+    expect(process.exit).toHaveBeenCalledTimes(1)
+  })
+})
--- __tests__/mocks/mock-params.ts
+++ __tests__/mocks/mock-params.ts
@@ -1,4 +1,4 @@
-export function mockParams(params: string[]): void {
+export function mockParams(...params: string[]): void {
   process.argv = ['node', 'index.[tj]s', 'command', ...params]
 }
 
