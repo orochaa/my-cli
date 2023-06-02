b4c6d19b75044f5069e30f6a20422f45b574d632
Bruno Rocha <brunorocha2674@gmail.com>
2023-06-02 11:49:13 -0300

    chore: complete clone's command description

--- src/commands/clone.ts
+++ src/commands/clone.ts
@@ -87,7 +87,7 @@ export function cloneRecord(app: App): void {
     name: 'clone',
     alias: null,
     params: ['<repository>'],
-    description: "Clone a Github's repository based on `setup`",
+    description: "Clone a Github's repository based on `setup`, sets git `origin` to `o`, install dependencies, and open it on vscode",
     example: 'my clone my-cli',
     action: cloneCommand
   })
