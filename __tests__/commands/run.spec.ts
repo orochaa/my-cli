828f9d581ee4692ffbd68bde16c8eeba11063f63
Bruno Rocha <brunorocha2674@gmail.com>
2023-08-01 12:47:25 -0300

    feat(run): custom scripts

--- __tests__/commands/run.spec.ts
+++ __tests__/commands/run.spec.ts
@@ -15,24 +15,6 @@ describe('run', () => {
   const sut = makeSut('run')
 
   describe('shallowRun', () => {
-    it('should verify package.json scripts', async () => {
-      jest.spyOn(JSON, 'parse').mockReturnValueOnce(null)
-
-      const promise = sut.exec('lint')
-
-      expect(promise).rejects.toThrow(NotFoundError)
-    })
-
-    it('should verify if script exists in package.json', async () => {
-      jest.spyOn(JSON, 'parse').mockReturnValueOnce({
-        scripts: {}
-      })
-
-      const promise = sut.exec('lint')
-
-      expect(promise).rejects.toThrow(NotFoundError)
-    })
-
     it('should run scripts', async () => {
       jest.spyOn(JSON, 'parse').mockReturnValueOnce({
         scripts: {
@@ -41,8 +23,140 @@ describe('run', () => {
         }
       })
 
-      await sut.exec('lint', 'build')
+      await sut.exec('lint build test')
 
+      expect(cp.execSync).toHaveBeenCalledTimes(3)
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        1,
+        'npm run lint',
+        expect.anything()
+      )
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        2,
+        'npm run build',
+        expect.anything()
+      )
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        3,
+        'npx test',
+        expect.anything()
+      )
+    })
+
+    it('should run custom scripts', async () => {
+      jest.spyOn(JSON, 'parse').mockReturnValueOnce({
+        scripts: {
+          lint: ' ',
+          build: ' '
+        }
+      })
+
+      await sut.execRaw('lint', 'build', 'vitest --run')
+
+      expect(cp.execSync).toHaveBeenCalledTimes(3)
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        1,
+        'npm run lint',
+        expect.anything()
+      )
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        2,
+        'npm run build',
+        expect.anything()
+      )
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        3,
+        'npx vitest --run',
+        expect.anything()
+      )
+    })
+
+    it('should run scripts partially', async () => {
+      jest.spyOn(JSON, 'parse').mockReturnValueOnce({
+        scripts: {
+          lint: ' ',
+          build: ' '
+        }
+      })
+
+      await sut.exec('lint build test -p')
+
+      expect(cp.execSync).toHaveBeenCalledTimes(2)
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        1,
+        'npm run lint',
+        expect.anything()
+      )
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        2,
+        'npm run build',
+        expect.anything()
+      )
+    })
+
+    it('should verify package.json', async () => {
+      jest.spyOn(JSON, 'parse').mockReturnValueOnce(null)
+
+      const promise = sut.exec('lint')
+
+      expect(promise).rejects.toThrow(NotFoundError)
+    })
+
+    it('should verify package.json scripts', async () => {
+      jest.spyOn(JSON, 'parse').mockReturnValueOnce({})
+
+      const promise = sut.exec('lint')
+
+      expect(promise).rejects.toThrow(NotFoundError)
+    })
+  })
+
+  describe('deepRun', () => {
+    beforeAll(() => {
+      writeFileSync(
+        join(cwd, 'scripts/package.json'),
+        JSON.stringify({
+          scripts: {
+            lint: ' ',
+            build: ' '
+          }
+        })
+      )
+    })
+
+    afterEach(() => {
+      process.chdir(cwd)
+    })
+
+    afterAll(() => {
+      rmSync(join(cwd, 'scripts/package.json'))
+    })
+
+    it('should run scripts', async () => {
+      await sut.exec('lint build test -d')
+
+      expect(cp.execSync).toHaveBeenCalledTimes(3)
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        1,
+        'npm run lint',
+        expect.anything()
+      )
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        2,
+        'npm run build',
+        expect.anything()
+      )
+      expect(cp.execSync).toHaveBeenNthCalledWith(
+        3,
+        'npx test',
+        expect.anything()
+      )
+    })
+
+    it('should run scripts partially(of each package.json)', async () => {
+      await sut.exec('lint build test -d -p')
+
+      expect(cp.execSync).toHaveBeenCalledTimes(2)
       expect(cp.execSync).toHaveBeenNthCalledWith(
         1,
         'npm run lint',
@@ -56,30 +170,28 @@ describe('run', () => {
     })
   })
 
-  describe('deepRun', () => {
-    beforeAll(() => {
-      writeFileSync(
-        join(cwd, '__tests__', 'package.json'),
-        JSON.stringify({
-          scripts: {
-            lint: ' ',
-            test: ' '
-          }
-        })
-      )
+  describe('promptRun', () => {
+    it('should verify package.json', async () => {
+      jest.spyOn(JSON, 'parse').mockReturnValueOnce(null)
+      expect(sut.exec()).rejects.toThrow(NotFoundError)
     })
 
-    afterEach(() => {
-      process.chdir(cwd)
+    it('should verify package.json scripts', async () => {
+      jest.spyOn(JSON, 'parse').mockReturnValueOnce({})
+      expect(sut.exec()).rejects.toThrow(NotFoundError)
     })
 
-    afterAll(() => {
-      rmSync(join(cwd, '__tests__', 'package.json'))
-    })
+    it("should run prompt's selected scripts", async () => {
+      jest.spyOn(JSON, 'parse').mockReturnValueOnce({
+        scripts: {
+          lint: ' ',
+          build: ' '
+        }
+      })
 
-    it('should run scripts of each package.json', async () => {
-      await sut.exec('lint', 'build', 'test', '-d')
+      await sut.exec()
 
+      expect(cp.execSync).toHaveBeenCalledTimes(2)
       expect(cp.execSync).toHaveBeenNthCalledWith(
         1,
         'npm run lint',
@@ -87,34 +199,6 @@ describe('run', () => {
       )
       expect(cp.execSync).toHaveBeenNthCalledWith(
         2,
-        'npm run test',
-        expect.anything()
-      )
-    })
-  })
-
-  describe('promptRun', () => {
-    it('should verify package.json', async () => {
-      jest.spyOn(JSON, 'parse').mockReturnValueOnce(null)
-
-      expect(sut.exec()).rejects.toThrow(NotFoundError)
-    })
-
-    it('should verify package.json scripts', async () => {
-      jest.spyOn(JSON, 'parse').mockReturnValueOnce({})
-
-      expect(sut.exec()).rejects.toThrow(NotFoundError)
-    })
-
-    it("should run prompt's selected scripts", async () => {
-      await sut.exec()
-
-      expect(cp.execSync).toHaveBeenCalledTimes(2)
-      expect(cp.execSync).toHaveBeenCalledWith(
-        'npm run lint',
-        expect.anything()
-      )
-      expect(cp.execSync).toHaveBeenCalledWith(
         'npm run build',
         expect.anything()
       )
--- __tests__/mocks/make-sut.ts
+++ __tests__/mocks/make-sut.ts
@@ -8,8 +8,6 @@ type Command = CommandKey extends `${infer TCommand}Record` ? TCommand : never
 
 export function makeSut(command: Command) {
   const sut = new App()
-  let isSilent = true
-
   for (const [key, record] of objectEntries(commands)) {
     if (key.startsWith(command)) {
       record(sut)
@@ -17,21 +15,28 @@ export function makeSut(command: Command) {
     }
   }
 
+  const setParams = (params: string[]): void => {
+    process.argv = ['node', 'index.[tj]s', 'command', ...params]
+  }
+
+  let isSilent = true
+  const setSilent = (): void => {
+    if (isSilent) {
+      process.argv.push('--silent')
+    } else {
+      isSilent = true
+    }
+  }
+
   return {
     exec: (...params: string[]): Promise<void> => {
-      process.argv = [
-        'node',
-        'index.[tj]s',
-        'command',
-        ...params.map(p => p.split(' ')).flat()
-      ]
-
-      if (isSilent) {
-        process.argv.push('--silent')
-      } else {
-        isSilent = true
-      }
-
+      setParams(params.map(p => p.split(' ')).flat())
+      setSilent()
+      return sut.exec(command)
+    },
+    execRaw: (...params: string[]): Promise<void> => {
+      setParams(params)
+      setSilent()
       return sut.exec(command)
     },
     enableLogs: (): void => {
