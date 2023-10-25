0adbefce98a939125e4933c7a5550590bac3bf2d
Bruno Rocha <brunorocha2674@gmail.com>
2023-10-25 11:44:41 -0300

    fix: ci

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,7 +10,8 @@ export const packageJsonPath = join(cwd, 'package.json')
 export const lockfileDir = join(homedir(), 'my-cli')
 export const lockfilePath = join(lockfileDir, 'setup-lock.json')
 
-export const root =
-  process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
-    ? join(dirname, '..', '..')
-    : join(dirname, '..')
+export const root = ['development', 'test', 'ci'].includes(
+  process.env.NODE_ENV!,
+)
+  ? join(dirname, '..', '..')
+  : join(dirname, '..')
