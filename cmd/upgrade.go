dbb558565046cc8225f86f98229c6fffe07e277a
Bruno Rocha <brunorocha2674@gmail.com>
2024-06-18 17:56:26 -0300

    feat: add upgrade command

---
+++ cmd/upgrade.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"context"
+
+	"github.com/Mist3rBru/go-clack/prompts"
+	"github.com/Mist3rBru/my-cli/internals/utils"
+	"github.com/spf13/cobra"
+)
+
+// upgradeCmd represents the upgrade command
+var upgradeCmd = &cobra.Command{
+	Use:     "upgrade",
+	Aliases: []string{"up"},
+	Short:   "Update package to latest version",
+	Long:    "Update package to latest version",
+	Run: func(cmd *cobra.Command, args []string) {
+		s, err := prompts.Spinner(context.Background(), prompts.SpinnerOptions{})
+		if err != nil {
+			prompts.Error(err.Error())
+			return
+		}
+		s.Start("Upgrading to lastest version")
+		utils.ExecOrExit("go install github.com/Mist3rBru/my-cli@latest")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(upgradeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// upgradeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// upgradeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
