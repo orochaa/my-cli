8a96c005d0cb319244d5a9aac1ef4fd15c1925b9
Bruno Rocha <brunorocha2674@gmail.com>
2024-08-09 23:46:32 -0300

    feat(copy): add task queue

--- cmd/copy.go
+++ cmd/copy.go
@@ -5,17 +5,29 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"regexp"
+	"runtime"
+	"slices"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/Mist3rBru/go-clack/prompts"
+	"github.com/Mist3rBru/go-clack/prompts/symbols"
+	"github.com/Mist3rBru/go-clack/third_party/picocolors"
+	"github.com/Mist3rBru/go-clack/third_party/sisteransi"
 	"github.com/Mist3rBru/my-cli/internals/lockfile"
 	"github.com/Mist3rBru/my-cli/internals/utils"
 	"github.com/spf13/cobra"
 )
+
+type CopyProgress struct {
+	Found  int
+	Copied int
+}
 
 // copyCmd represents the copy command
 var copyCmd = &cobra.Command{
@@ -23,77 +35,179 @@ var copyCmd = &cobra.Command{
 	Short: "Copy files from other project",
 	Long:  "Copy files from other project",
 	Run: func(cmd *cobra.Command, args []string) {
-		cwd, err := os.Getwd()
-		if err != nil {
-			prompts.Cancel(err.Error())
-			return
-		}
-
 		l := lockfile.Open()
 		defer l.Close()
 
 		userProjectsRootList := l.GetUserProjectsRootList()
-
 		selectedPaths, err := prompts.MultiSelectPath(prompts.MultiSelectPathParams{
-			Message:     "Select mudules to copy:",
+			Message:     "Select items to copy:",
 			InitialPath: userProjectsRootList[0],
 			Filter:      true,
 			Required:    true,
 		})
 		utils.VerifyPromptCancel(err)
+		os.Stdout.WriteString("\n")
 
-		wg := sync.WaitGroup{}
+		slices.Reverse(userProjectsRootList)
+		defer slices.Reverse(userProjectsRootList)
+
+		progressCh := make(chan CopyProgress)
+		taskCh := make(chan func())
 		startTime := time.Now()
 
-		for _, selectedPath := range selectedPaths {
-			wg.Add(1)
-
-			go func(selectedPath string) {
-				defer wg.Done()
-
-				var relativePath string
-				for _, root := range userProjectsRootList {
-					relativeRegex := regexp.MustCompile(fmt.Sprintf(`^%s/.+?/(.+)`, root))
-					if relativeRegex.MatchString(selectedPath) {
-						relativePath = relativeRegex.ReplaceAllString(selectedPath, "$1")
-						break
-					}
+		for range runtime.NumCPU() * 10 {
+			go func() {
+				for task := range taskCh {
+					task()
 				}
-
-				absoluteToPath := filepath.Join(cwd, relativePath)
-
-				stat, err := os.Stat(selectedPath)
-				if err != nil {
-					prompts.Error(err.Error())
-					return
-				}
-
-				if stat.IsDir() {
-					wg.Add(1)
-					go utils.CopyDir(selectedPath, absoluteToPath, &wg)
-				} else {
-					wg.Add(1)
-					go func() {
-						defer wg.Done()
-
-						folderPath := filepath.Join(cwd, filepath.Dir(relativePath))
-						if err := os.MkdirAll(folderPath, stat.Mode()); err != nil {
-							prompts.Error(err.Error())
-							return
-						}
-
-						if err := utils.CopyFile(selectedPath, absoluteToPath); err != nil {
-							prompts.Error(err.Error())
-							return
-						}
-					}()
-				}
-			}(selectedPath)
+			}()
 		}
 
-		wg.Wait()
-		prompts.Success("Files copied in " + time.Since(startTime).String())
+		var taskWg sync.WaitGroup
+		taskWg.Add(1)
+		taskCh <- func() {
+			defer taskWg.Done()
+			for _, selectedPath := range selectedPaths {
+				stat, err := os.Stat(selectedPath)
+				if err != nil {
+					continue
+				}
+
+				if !stat.IsDir() {
+					progressCh <- CopyProgress{Found: 1}
+					taskWg.Add(1)
+					taskCh <- func() {
+						defer taskWg.Done()
+						if err := RelativeCopyFile(userProjectsRootList, selectedPath); err != nil {
+							prompts.Error(err.Error())
+						} else {
+							progressCh <- CopyProgress{Copied: 1}
+						}
+					}
+					continue
+				}
+
+				MapDir(selectedPath, func(filePath string) {
+					progressCh <- CopyProgress{Found: 1}
+					taskWg.Add(1)
+					taskCh <- func() {
+						defer taskWg.Done()
+						if err := RelativeCopyFile(userProjectsRootList, filePath); err != nil {
+							prompts.Error(err.Error())
+						} else {
+							progressCh <- CopyProgress{Copied: 1}
+						}
+					}
+				})
+			}
+		}
+
+		go func() {
+			taskWg.Wait()
+			close(taskCh)
+			close(progressCh)
+		}()
+
+		var total, copied int
+		const barLength = 40
+
+		for p := range progressCh {
+			total += p.Found
+			copied += p.Copied
+			progress := copied * barLength / total
+			progressBar := picocolors.BgWhite(strings.Repeat(" ", progress))
+			remainingBar := strings.Repeat(" ", barLength-progress)
+			os.Stdout.WriteString(sisteransi.MoveCursor(-1, -999))
+			os.Stdout.WriteString(sisteransi.EraseCurrentLine())
+			os.Stdout.WriteString(fmt.Sprintf("%s %s%s %d/%d\n", picocolors.Gray(symbols.BAR_END), progressBar, remainingBar, copied, total))
+		}
+
+		os.Stdout.WriteString(sisteransi.MoveCursorUp(1))
+		os.Stdout.WriteString(sisteransi.EraseDown())
+		os.Stdout.WriteString(fmt.Sprintf("%s %s\n", picocolors.Gray(symbols.BAR_END), picocolors.Dim(fmt.Sprintf("%d files copied in %s", copied, time.Since(startTime).String()))))
 	},
+}
+
+func MapDir(dirPath string, cb func(filePath string)) {
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		prompts.Error(err.Error())
+		return
+	}
+
+	for _, entry := range entries {
+		if entry.Name() == ".git" || entry.Name() == "node_modules" {
+			continue
+		}
+		entryPath := filepath.Join(dirPath, entry.Name())
+
+		if entry.IsDir() {
+			MapDir(entryPath, cb)
+			continue
+		}
+
+		cb(entryPath)
+	}
+}
+
+func RelativeCopyFile(userProjectsRootList []string, filePath string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	var relativePath string
+	for _, root := range userProjectsRootList {
+		relativeRegex := regexp.MustCompile(fmt.Sprintf(`^%s/.+?/(.+)`, root))
+		if relativeRegex.MatchString(filePath) {
+			relativePath = relativeRegex.ReplaceAllString(filePath, "$1")
+			break
+		}
+	}
+
+	if relativePath == "" {
+		relativePath = filepath.Base(filePath)
+	}
+
+	fromDirPath := filepath.Dir(filePath)
+	toFilePath := filepath.Join(cwd, relativePath)
+	toDirPath := filepath.Dir(toFilePath)
+
+	fromDirStat, err := os.Stat(fromDirPath)
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(toDirPath, fromDirStat.Mode()); err != nil {
+		return err
+	}
+
+	if err = CopyFile(filePath, toFilePath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CopyFile(fromFilePath, toFilePath string) error {
+	fromFile, err := os.Open(fromFilePath)
+	if err != nil {
+		return err
+	}
+	defer fromFile.Close()
+
+	toFile, err := os.Create(toFilePath)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	_, err = io.Copy(toFile, fromFile)
+	if err != nil {
+		return err
+	}
+
+	return toFile.Sync()
 }
 
 func init() {
--- internals/utils/fs.go
+++ internals/utils/fs.go
@@ -2,13 +2,7 @@ package utils
 
 import (
 	"encoding/json"
-	"io"
-	"io/fs"
 	"os"
-	"path/filepath"
-	"sync"
-
-	"github.com/Mist3rBru/go-clack/prompts"
 )
 
 func ReadJson(path string, v any) error {
@@ -31,62 +25,4 @@ func WriteJson(path string, v any) error {
 func Exists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
-}
-
-func CopyFile(fromFilePath, toFilePath string) error {
-	fromFile, err := os.Open(fromFilePath)
-	if err != nil {
-		return err
-	}
-	defer fromFile.Close()
-
-	toFile, err := os.Create(toFilePath)
-	if err != nil {
-		return err
-	}
-	defer toFile.Close()
-
-	_, err = io.Copy(toFile, fromFile)
-	if err != nil {
-		return err
-	}
-
-	return toFile.Sync()
-}
-
-func CopyDir(fromDirPath string, toDirPath string, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	if err := os.MkdirAll(toDirPath, fs.ModeDir); err != nil {
-		prompts.Error(err.Error())
-		return
-	}
-
-	entries, err := os.ReadDir(fromDirPath)
-	if err != nil {
-		prompts.Error(err.Error())
-		return
-	}
-
-	for _, entry := range entries {
-		if entry.Name() == ".git" || entry.Name() == "node_modules" {
-			continue
-		}
-
-		fromPath := filepath.Join(fromDirPath, entry.Name())
-		toPath := filepath.Join(toDirPath, entry.Name())
-
-		if entry.IsDir() {
-			wg.Add(1)
-			go CopyDir(fromPath, toPath, wg)
-		} else {
-			wg.Add(1)
-			go func(fromFilePath, toFilePath string) {
-				defer wg.Done()
-				if err = CopyFile(fromFilePath, toFilePath); err != nil {
-					prompts.Error(err.Error())
-				}
-			}(fromPath, toPath)
-		}
-	}
 }
